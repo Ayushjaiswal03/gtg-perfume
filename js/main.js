2856bdaa0d197a909b00ba5e14b13722c71af619
Ayushjaiswal03 <ayush60000@gmail.com>
2025-12-30 01:20:56 +0530

    header section finalized

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ document.addEventListener('DOMContentLoaded', () => {
   initCounter();
   initAccordion();
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.hamburger');
+  const mobileMenu = document.querySelector('.mobile-menu');
+
+  hamburger.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+  });
+});
